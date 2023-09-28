909fbd909d43270e55ab9aff3b118d2b7de4e2d6
Juan Alcoba <juanchoalcoba@gmail.com>
2023-09-28 10:53:08 -0300

    add details

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -30,7 +30,7 @@ const HeroSection = () => {
             <div className='grid grid-cols-1 sm:grid-cols-12'>
                 <div className='col-span-7 place-self-center text-center sm:text-left'>
                     <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
-                        <span className='text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-pink-900'>
+                        <span className='text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-gray-200'>
                             Hello, I´m{" "}
                         </span>
                         <br />
